7f50cb862a575bd016356c1e9f3b962c63beef64
SunQ <sunq0313@gmail.com>
2018-07-28 23:51:35 +0800

    remove input normalization

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -24,9 +24,6 @@ class SVHNDataLoader(BaseDataLoader):
 
         trsfm = transforms.Compose([
             transforms.ToTensor(),
-            # Normalization that every pytorch pretrained models expect
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  std=[0.229, 0.224, 0.225])
         ])
         self.dataset = datasets.SVHN(data_dir, split='train', transform=trsfm, download=True)
         super(SVHNDataLoader, self).__init__(self.dataset, self.batch_size, self.valid_batch_size, shuffle, validation_split, validation_fold, num_workers)
@@ -44,9 +41,6 @@ class CocoDataLoader(BaseDataLoader):
             transforms.CenterCrop(256),
             transforms.Resize(64),
             transforms.ToTensor(),
-            # Normalization that every pytorch pretrained models expect
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  std=[0.229, 0.224, 0.225]),
         ])
         
         self.dataset = CocoWrapper(data_dir, transform=trsfm)
@@ -64,9 +58,6 @@ class CubDataLoader(BaseDataLoader):
             transforms.CenterCrop(256),
             # transforms.Resize(64),
             transforms.ToTensor(),
-            # Normalization that every pytorch pretrained models expect
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                  std=[0.229, 0.224, 0.225])
         ])
         
         self.dataset = CubDataset(data_dir, transform=trsfm)
